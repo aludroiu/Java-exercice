dd00d8fc5484053eca585d1a6a75eccfa01d7c0c
Alexandra <aludroiu@deloittece.com>
2023-07-24 14:04:11 +0300

    add ints

--- src/java/variables/CountComment.java
+++ src/java/variables/CountComment.java
@@ -2,15 +2,17 @@ package java.variables;
 
 //This is the class declaration
 public class CountComment {
-    //This is the main method that runs when you compile
+
     public static void main(String[] args) {
-        //This is where you will define your variable
-
-        //This is where you will print your variable
-
+        int numComments = 6;
+        System.out.println(numComments);
+        /*
+        This is where you will define your variable
+        This is outside the class
+        This is where you will print your variable
+        This is the end of the class
+        This is the main method that runs when you compile
+        */
     }
-
-    //This is the end of the class
 }
 
-//This is outside the class
