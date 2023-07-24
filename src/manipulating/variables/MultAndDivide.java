ebea3c616b9131dd5cb21aece2c1a99eda709618
Alexandra <aludroiu@deloittece.com>
2023-07-24 15:52:23 +0300

    Print the total variable - 32.625

---
+++ src/manipulating/variables/MultAndDivide.java
@@ -0,0 +1,11 @@
+package manipulating.variables;
+
+public class MultAndDivide {
+    public static void main(String[] args) {
+        double subtotal = 30;
+        double tax = 0.0875;
+        double total = subtotal + subtotal * tax;
+
+        System.out.println(total);
+    }
+}
