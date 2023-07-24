56b1fadedbb772634e54cb554e0925e899cf6adf
Alexandra <aludroiu@deloittece.com>
2023-07-24 15:36:58 +0300

    Print the remaining zebras

--- src/manipulating/variables/PlusAndMinus.java
+++ src/manipulating/variables/PlusAndMinus.java
@@ -2,11 +2,14 @@ package manipulating.variables;
 
 public class PlusAndMinus {
     public static void main(String[] args) {
-        int zebrasInZoo = 8;
+        int zebrasInZoo = 6;
         int giraffesInZoo = 4;
         int animalsInZoo = zebrasInZoo + giraffesInZoo;
 
-        System.out.println(animalsInZoo);
+        //System.out.println(animalsInZoo);
+
+        int numZebrasAfterTrade = zebrasInZoo--;
+        System.out.println(numZebrasAfterTrade);
 
     }
 }
