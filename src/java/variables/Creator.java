3b74bb0b861145c8299eef3d6a739e149a895b3f
Alexandra <aludroiu@deloittece.com>
2023-07-24 14:01:45 +0300

    Add var

--- src/java/variables/Creator.java
+++ src/java/variables/Creator.java
@@ -1,17 +1,9 @@
 package java.variables;
-
-//This is the class declaration
-public class CountComment {
+public class Creator {
     public static void main(String[] args) {
-        int numComments = 6;
-        System.out.println(numComments);
-        /*
-        This is where you will define your variable
-        This is where you will print your variable
-        This is outside the class
-        This is the end of the class
-        This is the main method that runs when you compile
-        */
+        String name = "James Gosling";
+        int yearCreated = 1995;
+        System.out.println(name);
+        System.out.println(yearCreated);
     }
 }
-
