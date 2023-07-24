dadd26c592f06f94d73895662c0389526badfd34
Alexandra <aludroiu@deloittece.com>
2023-07-24 16:04:43 +0300

    add correct compound assignment operators

---
+++ src/manipulating/variables/BakeSale.java
@@ -0,0 +1,14 @@
+package manipulating.variables;
+
+public class BakeSale {
+    public static void main(String[] args) {
+        int numCookies = 17;
+        numCookies-=3;
+        //results is 14
+        numCookies/=2;
+        //results is 7
+
+        // Add your code above
+        System.out.println(numCookies);
+    }
+}
