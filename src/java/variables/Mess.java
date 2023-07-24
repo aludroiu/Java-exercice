f630a627454a9be5631d3dae7fbecb909be72a70
Alexandra <aludroiu@deloittece.com>
2023-07-24 14:48:44 +0300

    Add new and correct data

--- src/java/variables/Mess.java
+++ src/java/variables/Mess.java
@@ -2,10 +2,10 @@ package java.variables;
 
 public class Mess {
     public static void main(String[] args) {
-        String year = 2001;
-        double title = "Shrek";
-        int genre = 'C';
-        boolean runtime = 1.58;
-        char isPG = true;
+        int year = 2001;
+        String title = "Shrek";
+        char genre = 'C';
+        double runtime = 1.58;
+        boolean isPG = true;
     }
 }
