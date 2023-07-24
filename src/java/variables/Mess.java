929eac298c69515d0d65b774bb2381cf8e360c4f
Alexandra <aludroiu@deloittece.com>
2023-07-24 14:46:27 +0300

    Add wrong data

---
+++ src/java/variables/Mess.java
@@ -0,0 +1,11 @@
+package java.variables;
+
+public class Mess {
+    public static void main(String[] args) {
+        String year = 2001;
+        double title = "Shrek";
+        int genre = 'C';
+        boolean runtime = 1.58;
+        char isPG = true;
+    }
+}
