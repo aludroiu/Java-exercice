0bc561f465044deba9d7afbebdcce788fc41e00a
Alexandra <aludroiu@deloittece.com>
2023-07-25 11:51:25 +0300

    add String parameter product to the Store constructor

--- src/Objects/classes/Store.java
+++ src/Objects/classes/Store.java
@@ -1,20 +1,16 @@
 package Objects.classes;
 
 public class Store {
-    // declare instance fields here!
+    // instance fields
     String productType;
 
     // constructor method
-    public Store() {
-        System.out.println("I am inside the constructor method.");
+    public Store(String product) {
+        productType = product;
     }
 
     // main method
     public static void main(String[] args) {
-        System.out.println("This code is inside the main method.");
 
-        Store lemonadeStand = new Store();
-
-        System.out.println(lemonadeStand);
     }
 }
