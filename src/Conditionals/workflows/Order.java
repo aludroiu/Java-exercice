cad60d0547a86575b3c92fc0332caf4eebe2896d
Alexandra <aludroiu@deloittece.com>
2023-07-25 15:50:36 +0300

    add if then else statement

---
+++ src/Conditionals/workflows/Order.java
@@ -0,0 +1,18 @@
+package Conditionals.workflows;
+
+public class Order {
+
+    public static void main(String[] args) {
+
+        boolean isFilled = false;
+
+        // Write an if-then-else statement:
+
+        if (isFilled){
+            System.out.println("Shipping");}
+        else{
+            System.out.println("Order not ready");
+        }
+
+    }
+}
