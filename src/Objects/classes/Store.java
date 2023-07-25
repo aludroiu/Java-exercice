1a1353b1c414459d120d7a07f9a06899b2fe85d2
Alexandra <aludroiu@deloittece.com>
2023-07-25 11:00:29 +0300

    add String instance field for productType

--- src/Objects/classes/Store.java
+++ src/Objects/classes/Store.java
@@ -1,20 +1,20 @@
 package Objects.classes;
 
 public class Store {
+    // declare instance fields here!
+    String productType;
 
-    // new method: constructor!
+    // constructor method
     public Store() {
         System.out.println("I am inside the constructor method.");
     }
 
-    // main method is where we create instances!
+    // main method
     public static void main(String[] args) {
-        System.out.println("Start of the main method.");
+        System.out.println("This code is inside the main method.");
 
-        // create the instance below
         Store lemonadeStand = new Store();
 
-        // print the instance below
         System.out.println(lemonadeStand);
     }
 }
