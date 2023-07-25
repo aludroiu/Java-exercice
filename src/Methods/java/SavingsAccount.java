469f58f1ebd01441a940011d2bf3a7c14e6d9477
Alexandra <aludroiu@deloittece.com>
2023-07-25 15:03:08 +0300

    add review practice

--- src/Methods/java/SavingsAccount.java
+++ src/Methods/java/SavingsAccount.java
@@ -1,5 +1,3 @@
-package Methods.java;
-
 public class SavingsAccount {
 
     int balance;
@@ -8,40 +6,50 @@ public class SavingsAccount {
         balance = initialBalance;
     }
 
+    public void checkBalance(){
+        System.out.println("Hello!");
+        System.out.println("Your balance is "+balance);
+    }
+
+    public void deposit(int amountToDeposit){
+        balance = amountToDeposit + balance;
+        System.out.println("You just deposited " + amountToDeposit);
+    }
+
+    public int withdraw(int amountToWithdraw){
+        balance = balance - amountToWithdraw;
+        System.out.println("You just withdrew " + amountToWithdraw);
+        return amountToWithdraw;
+    }
+
+    public String toString(){
+        return "This is a savings account with " + balance + " saved.";
+    }
+
     public static void main(String[] args){
         SavingsAccount savings = new SavingsAccount(2000);
 
         //Check balance:
-        System.out.println("Hello!");
-        System.out.println("Your balance is "+savings.balance);
+        savings.checkBalance();
 
         //Withdrawing:
-        int afterWithdraw = savings.balance - 300;
-        savings.balance = afterWithdraw;
-        System.out.println("You just withdrew "+300);
+        savings.withdraw(300);
 
         //Check balance:
-        System.out.println("Hello!");
-        System.out.println("Your balance is "+savings.balance);
+        savings.checkBalance();
 
         //Deposit:
-        int afterDeposit = savings.balance + 600;
-        savings.balance = afterDeposit;
-        System.out.println("You just deposited "+600);
+        savings.deposit(600);
 
         //Check balance:
-        System.out.println("Hello!");
-        System.out.println("Your balance is "+savings.balance);
+        savings.checkBalance();
 
         //Deposit:
-        int afterDeposit2 = savings.balance + 600;
-        savings.balance = afterDeposit2;
-        System.out.println("You just deposited "+600);
+        savings.deposit(600);
 
         //Check balance:
-        System.out.println("Hello!");
-        System.out.println("Your balance is "+savings.balance);
+        savings.checkBalance();
 
+        System.out.println(savings);
     }
 }
-
