c78ea9ea2d3da2a53a7029ae07f5aeba6297f754
Alexandra <aludroiu@deloittece.com>
2023-07-25 12:17:53 +0300

    Add two new instance fields for Store. inventoryCount of type int. inventoryPrice of type double

--- src/Objects/classes/Store.java
+++ src/Objects/classes/Store.java
@@ -1,6 +1,6 @@
 package Objects.classes;
 
-public class Store {
+/*public class Store {
     // instance fields
     String productType;
 
@@ -13,5 +13,25 @@ public class Store {
     public static void main(String[] args) {
         Store lemonadeStand = new Store("lemonade");
         System.out.println(lemonadeStand.productType);
+    }
+}*/
+
+public class Store {
+    // instance fields
+    String productType;
+    //new instance field
+    int inventoryCount;
+    double inventoryPrice;
+
+    // constructor method
+    public Store(String product, int count, double price) {
+        productType = product;
+        inventoryCount = count;
+        inventoryPrice = price;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        Store cookieShop = new Store("cookies", 12, 3.75);
     }
 }
