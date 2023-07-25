88d8408d7f60f19f082548ae8752b4c1a006f7f7
Alexandra <aludroiu@deloittece.com>
2023-07-25 14:18:20 +0300

    Add new method called getPriceWithTax()

--- src/Methods/java/Store.java
+++ src/Methods/java/Store.java
@@ -1,57 +1,4 @@
 package Methods.java;
-
-/*public class Store {
-    // instance fields
-    String productType;
-
-    // constructor method
-    public Store(String product) {
-        productType = product;
-    }
-
-    // advertise method
-    public void advertise() {
-        System.out.println("Selling " + productType + "!");
-        System.out.println("Come spend some money!");
-    }
-
-    // main method
-    public static void main(String[] args) {
-        Store lemonadeStand = new Store("Lemonade");
-        lemonadeStand.advertise();
-        lemonadeStand.advertise();
-        lemonadeStand.advertise();
-    }
-}
- */
-
-/*
-public class Store {
-    // instance fields
-    String productType;
-
-    public void greetCustomer(String customer){
-        System.out.println("Welcome to the store, " + customer + "!");
-    }
-
-    // constructor method
-    public Store(String product) {
-        productType = product;
-    }
-
-    // advertise method
-    public void advertise() {
-        String message = "Selling " + productType + "!";
-        System.out.println(message);
-    }
-
-    // main method
-    public static void main(String[] args) {
-        Store lemonadeStand = new Store("Lemonade");
-        lemonadeStand.greetCustomer("Laura");
-    }
-}*/
-
 public class Store {
     // instance fields
     String productType;
@@ -67,14 +14,18 @@ public class Store {
     public void increasePrice(double priceToAdd){
         double newPrice = price + priceToAdd;
         price = newPrice;
-
-
     }
 
+    // get price with tax method
+    public double getPriceWithTax(){
+        double totalPrice = price + price * 0.08;
+        return totalPrice;
+    }
     // main method
     public static void main(String[] args) {
         Store lemonadeStand = new Store("Lemonade", 3.75);
-        lemonadeStand.increasePrice(1.5);
-        System.out.println(lemonadeStand.price);
+        double lemonadePrice = lemonadeStand.getPriceWithTax();
+        System.out.println(lemonadePrice);
+
     }
 }
