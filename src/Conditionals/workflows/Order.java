569cfb289a57f56432d7f38983813b6307a0aa66
Alexandra <aludroiu@deloittece.com>
2023-07-25 16:21:28 +0300

    use switch statement

--- src/Conditionals/workflows/Order.java
+++ src/Conditionals/workflows/Order.java
@@ -2,16 +2,14 @@ public class Order {
     boolean isFilled;
     double billAmount;
     String shipping;
-    String couponCode;
 
-    public Order(boolean filled, double cost, String shippingMethod, String coupon) {
+    public Order(boolean filled, double cost, String shippingMethod) {
         if (cost > 24.00) {
             System.out.println("High value item!");
         }
         isFilled = filled;
         billAmount = cost;
         shipping = shippingMethod;
-        couponCode = coupon;
     }
 
     public void ship() {
@@ -24,24 +22,26 @@ public class Order {
     }
 
     public double calculateShipping() {
-        if (shipping.equals("Regular")) {
-            return 0;
-        } else if (shipping.equals("Express")) {
-            // Add your code here
-            if (couponCode.equals("ship50")) {
-                return 0.85;
-            } else {
-                return 1.75;
-            }
-        } else {
-            return .50;
+        double shippingCost;
+        // declare switch statement here
+        switch (shipping) {
+            case "Regular":
+                shippingCost = 0;
+                break;
+            case "Express":
+                shippingCost = 1.75;
+                break;
+            default:
+                shippingCost = .50;
         }
+
+        return shippingCost;
     }
 
     public static void main(String[] args) {
         // do not alter the main method!
-        Order book = new Order(true, 9.99, "Express", "ship50");
-        Order chemistrySet = new Order(false, 72.50, "Regular", "freeShipping");
+        Order book = new Order(true, 9.99, "Express");
+        Order chemistrySet = new Order(false, 72.50, "Regular");
 
         book.ship();
         chemistrySet.ship();
