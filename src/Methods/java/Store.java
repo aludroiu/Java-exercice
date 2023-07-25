6da250fee5005500f8f1c1b67f6258f88ffbc53e
Alexandra <aludroiu@deloittece.com>
2023-07-25 14:06:05 +0300

    Add price instance

--- src/Methods/java/Store.java
+++ src/Methods/java/Store.java
@@ -25,6 +25,7 @@ package Methods.java;
 }
  */
 
+/*
 public class Store {
     // instance fields
     String productType;
@@ -48,5 +49,32 @@ public class Store {
     public static void main(String[] args) {
         Store lemonadeStand = new Store("Lemonade");
         lemonadeStand.greetCustomer("Laura");
+    }
+}*/
+
+public class Store {
+    // instance fields
+    String productType;
+    double price;
+
+    // constructor method
+    public Store(String product, double initialPrice) {
+        productType = product;
+        price = initialPrice;
+    }
+
+    // increase price method
+    public void increasePrice(double priceToAdd){
+        double newPrice = price + priceToAdd;
+        price = newPrice;
+
+
+    }
+
+    // main method
+    public static void main(String[] args) {
+        Store lemonadeStand = new Store("Lemonade", 3.75);
+        lemonadeStand.increasePrice(1.5);
+        System.out.println(lemonadeStand.price);
     }
 }
