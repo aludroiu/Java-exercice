4caaf292624a091771b4f90685a06223ade0f8bb
Alexandra <aludroiu@deloittece.com>
2023-07-25 14:29:07 +0300

    add string toString method

--- src/Methods/java/Store.java
+++ src/Methods/java/Store.java
@@ -21,6 +21,10 @@ public class Store {
         double totalPrice = price + price * 0.08;
         return totalPrice;
     }
+    public String toString(){
+        return "This store sells " + productType + " at a price of " + price + ".";
+    }
+
     // main method
     public static void main(String[] args) {
         Store lemonadeStand = new Store("Lemonade", 3.75);
