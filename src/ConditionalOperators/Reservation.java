aeb7ca1064cef4cedfdd645e385c776904de9fe4
Alexandra <aludroiu@deloittece.com>
2023-07-26 11:13:52 +0300

    add conditional OR inside Reservation

--- src/ConditionalOperators/Reservation.java
+++ src/ConditionalOperators/Reservation.java
@@ -6,6 +6,10 @@ public class Reservation {
     boolean isConfirmed;
 
     public Reservation(int count, int capacity, boolean open) {
+        // Write conditional statement below
+        if (count < 1 || count > 8){
+            System.out.println("Invalid reservation!");
+        }
         guestCount = count;
         restaurantCapacity = capacity;
         isRestaurantOpen = open;
