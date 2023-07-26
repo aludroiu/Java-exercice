52370a98cdbee7b80eaeb644cfe9c6a444fdbcec
Alexandra <aludroiu@deloittece.com>
2023-07-26 11:31:22 +0300

    add conditional NOT inside informUser method

--- src/ConditionalOperators/Reservation.java
+++ src/ConditionalOperators/Reservation.java
@@ -39,7 +39,12 @@ public class Reservation {
     }
 
     public void informUser() {
-        System.out.println("Please enjoy your meal!");
+        // Write conditional here
+        if (!isConfirmed) {
+            System.out.println("Unable to confirm reservation, please contact restaurant.");
+        } else {
+            System.out.println("Please enjoy your meal!");
+        }
     }
 
     public static void main(String[] args) {
