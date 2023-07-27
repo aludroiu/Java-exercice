852e2c16a598fb4de9aebc4d3e56c22ad0b7eaf6
Alexandra <aludroiu@deloittece.com>
2023-07-27 12:09:39 +0300

    Add new array list

---
+++ src/JavaArrays/ToDos.java
@@ -0,0 +1,12 @@
+package JavaArrays;
+// import the ArrayList package here:
+import java.util.ArrayList;
+
+class ToDos {
+
+    public static void main(String[] args) {
+
+        // Create toDoList below:
+        ArrayList<String> toDoList = new ArrayList<String>();
+    }
+}
